9bba1ef9ce49155a57dd104b48f90ff81233a6fe
Charles maina <muragemaina09@gmail.com>
2019-05-10 11:24:32 +0300

    ran first test for creating new user

--- password_test.py
+++ password_test.py
@@ -1,7 +1,7 @@
 import unittest
-from password import password
+from password import Password
 
-class TestPassword = (unittest.TestCase):
+class TestPassword(unittest.TestCase):
     def setUp(self):
         self.new_password = Password("charles","maina","charles94","3094")
 
@@ -10,6 +10,8 @@ class TestPassword = (unittest.TestCase):
         self.assertEqual(self.new_password.last_name,"maina")
         self.assertEqual(self.new_password.user_name,"charles94")
         self.assertEqual(self.new_password.password,"3094")
+
+    def test_save_password()
 
 if __name__ == '__main__':
     unittest.main()
