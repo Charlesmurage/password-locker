3249e9f6701405d298aacab5013934471a81e729
Charles maina <muragemaina09@gmail.com>
2019-05-10 11:44:44 +0300

    ran test for saving multiple save password

--- password_test.py
+++ password_test.py
@@ -3,20 +3,23 @@ from password import Password
 
 class TestPassword(unittest.TestCase):
     def setUp(self):
+
         self.new_password = Password("charles","maina","charles94","3094")
 
+    def tearDown(self):
+            Password.password_list = []
     def test_init(self):
-        self.assertEqual(self.new_password.first_name,"charles")
-        self.assertEqual(self.new_password.last_name,"maina")
-        self.assertEqual(self.new_password.user_name,"charles94")
-        self.assertEqual(self.new_password.password,"3094")
+            self.assertEqual(self.new_password.first_name,"charles")
+            self.assertEqual(self.new_password.last_name,"maina")
+            self.assertEqual(self.new_password.user_name,"charles94")
+            self.assertEqual(self.new_password.password,"3094")
 
     def test_save_password(self):
-        self.new_password.save_password()
-        self.assertEqual(len(Password.password_list),1)
+            self.new_password.save_password()
+            self.assertEqual(len(Password.password_list),1)
 
     def test_save_mulitple_password(self):
-        self.new_password.save_password()
+            self.new_password.save_password()
             test_password = Password("charles","maina","charles34","1234") 
             test_password.save_password()
             self.assertEqual(len(Password.password_list),2)
